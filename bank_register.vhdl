f04b5ffbf5a83732b4738ee3b016ec297bced96d
James Ots <code@jamesots.com>
2017-05-07 07:24:31 +0100

    Add reset to banks

--- bank_register.vhdl
+++ bank_register.vhdl
@@ -3,10 +3,11 @@ use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.ALL;
 
 entity bank_register is
-    port ( d   : in  std_logic_vector(7 downto 0);
-           clk : in  std_logic;
-           oe  : in  std_logic;
-           b   : out std_logic_vector(7 downto 0));
+    port ( d     : in  std_logic_vector(7 downto 0);
+           clk   : in  std_logic;
+           oe    : in  std_logic;
+           b     : out std_logic_vector(7 downto 0);
+			  reset : in  std_logic);
 end bank_register;
 
 architecture behavioral of bank_register is
@@ -14,9 +15,13 @@ architecture behavioral of bank_register is
 begin
    process (clk, d) is
    begin
-      if clk'event and clk = '1' then
-         data <= d;
-      end if;
+		if reset = '0' then
+         data <= "11000000";
+		else
+			if clk'event and clk = '1' then
+				data <= d;
+			end if;
+		end if;
    end process;
    
    b <= data when oe = '0';
