97dd3a0064e0998b1b9ba72dd9658f8a3f2ba730
James Ots <james.ots@i-nexus.com>
2017-05-09 12:06:23 +0100

    Fix formatting

--- bank_register.vhdl
+++ bank_register.vhdl
@@ -6,7 +6,7 @@ entity bank_register is
     port ( d     : in  std_logic_vector(7 downto 0);
            clk   : in  std_logic;
            b     : out std_logic_vector(7 downto 0);
-			  reset : in  std_logic);
+           reset : in  std_logic);
 end bank_register;
 
 architecture behavioral of bank_register is
@@ -14,13 +14,13 @@ architecture behavioral of bank_register is
 begin
    process (clk, reset) is
    begin
-		if reset = '1' then
+      if reset = '1' then
          data <= "11000000";
-		else
-			if clk'event and clk = '1' then
-				data <= d;
-			end if;
-		end if;
+      else
+         if clk'event and clk = '1' then
+            data <= d;
+         end if;
+      end if;
    end process;
    
    b <= data;
