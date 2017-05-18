60ee84403e8665a2e3bbfff3dedaa92fd1cd38f1
James Ots <code@jamesots.com>
2017-05-18 20:53:36 +0100

    Add ability to read from FTDI via in IN, instead of memory mapping it

--- z80glue.vhdl
+++ z80glue.vhdl
@@ -39,6 +39,10 @@ entity z80glue is
            rom_oe_n   : out   std_logic;
            rom_ce_n   : out   std_logic;
            
+           rtc_we_n   : out   std_logic;
+           rtc_oe_n   : out   std_logic;
+           rtc_ce_n   : out   std_logic;
+           
            scr_rs     : out   std_logic;
            scr_rw     : out   std_logic;
            scr_e_n    : out   std_logic;
@@ -107,11 +111,16 @@ architecture behavioral of z80glue is
    
    signal bank_i : std_logic_vector(7 downto 0);
    
-   signal sel    : std_logic_vector(7 downto 0);
-   signal decoder_oe : std_logic;
+   signal sel0    : std_logic_vector(7 downto 0);
+   signal sel1    : std_logic_vector(7 downto 0);
+   signal decoder0_oe : std_logic;
+   signal decoder1_oe : std_logic;
    
    signal mem_rd_n  : std_logic;
    signal mem_wr_n  : std_logic;
+   
+   signal io_rd_n  : std_logic;
+   signal io_wr_n  : std_logic;
    
    signal ftdi_rd_n_i : std_logic;
    signal ftdi_wr_n_i : std_logic;
@@ -119,9 +128,11 @@ architecture behavioral of z80glue is
    signal reset_i   : std_logic;
    signal long_reset_n_i : std_logic;
 
-   type selection is
+   type selection0 is
       (sel_bank0, sel_bank1, sel_bank2, sel_bank3,
-       sel_screen_inst, sel_screen_data, sel_bell, sel_sd);       
+       sel_screen_inst, sel_screen_data, sel_bell, sel_sd);
+   type selection1 is
+      (sel_ftdi_data, sel_ftdi_status, sel_rtc);
        
    signal scr_sel    : std_logic;
    signal scr_wait_n : std_logic;
@@ -143,14 +154,17 @@ begin
    reset_out_n <= long_reset_n_i;
    
    -- decoder_oe enables the decoder output when an IO request is happening on 00000XXX.
-   decoder_oe <= not(a(7) or a(6) or a(5) or a(4) or a(3) or iorq_n);
-   c_decoder: decoder port map (a(2 downto 0), decoder_oe, sel);
+   decoder0_oe <= not(a(7) or a(6) or a(5) or a(4) or a(3) or iorq_n);
+   c_decoder0: decoder port map (a(2 downto 0), decoder0_oe, sel0);
+   
+   decoder1_oe <= not(a(7) or a(6) or a(5) or a(4) or not(a(3)) or iorq_n);
+   c_decoder1: decoder port map (a(2 downto 0), decoder1_oe, sel1);
    
    -- bankX_wr is high when trying to do an OUT to the bank's address
-   bank0_wr <= sel(0) and not(wr_n);
-   bank1_wr <= sel(1) and not(wr_n);
-   bank2_wr <= sel(2) and not(wr_n);
-   bank3_wr <= sel(3) and not(wr_n);
+   bank0_wr <= sel0(0) and not(wr_n);
+   bank1_wr <= sel0(1) and not(wr_n);
+   bank2_wr <= sel0(2) and not(wr_n);
+   bank3_wr <= sel0(3) and not(wr_n);
    
    bank_0: bank_register port map (d, bank0_wr, bank0, reset_i);
    bank_1: bank_register port map (d, bank1_wr, bank1, reset_i);
@@ -160,30 +174,33 @@ begin
    -- bank_i should be set at all times, depending on what is on a14 and 15
    c_bank_multiplex: bank_multiplex port map (clk4_i, a(15 downto 14), bank0, bank1, bank2, bank3, bank_i);
    
-   bell_sel <= not(wr_n) and sel(6);
+   bell_sel <= not(wr_n) and sel0(6);
    c_bell_latch: bell_latch port map (bell_sel, d(0), reset_i, bell);
    
-   scr_sel <= sel(4) or sel(5);
+   scr_sel <= sel0(4) or sel0(5);
    c_screen_writer: screen_writer port map (clk4_i, scr_sel, scr_e_n, scr_wait_n);
    scr_rs <= a(0);
    scr_rw <= wr_n;
 
-   ftdi_sel_n <= not(bank_i(7)) or not(bank_i(6)) or mreq_n;     -- ftdi = 11xxxxxx
+   ftdi_sel_n <= (not(bank_i(7)) or not(bank_i(6)) or mreq_n);   -- ftdi = 11xxxxxx
    ram_sel_n <= bank_i(7) or mreq_n;                             -- ram  = 0xxxxxxx
    rom_sel_n <= not(bank_i(7)) or bank_i(6) or mreq_n;           -- rom  = 10xxxxxx
---   ftdi_sel_n <= '0';
---   ram_sel_n <= '1';
---   rom_sel_n <= '1';
    
    mem_rd_n <= rd_n or mreq_n;
    mem_wr_n <= wr_n or mreq_n;
+   
+   io_rd_n <= rd_n or iorq_n;
+   io_wr_n <= wr_n or iorq_n;
    
 --   ftdi_wr_n_i <= mem_wr_n or ftdi_sel_n;
 --   ftdi_wr_n <= ftdi_wr_n_i;
    ftdi_wr_n <= '1';
 
+--   rd_n or ((mreq_n or ftdi_sel_n) and not(sel1))
    -- try to read from the ftdi if we're doing a memory read and the ftdi is selected
-   ftdi_rd_n_i <= mreq_n or rd_n or ftdi_sel_n;
+   ftdi_rd_n_i <= rd_n or ((mreq_n or ftdi_sel_n)
+      -- or we're doing an IO read on the ftdi port (port 8)
+      and (not sel1(0)));
    ftdi_rd_n <= ftdi_rd_n_i;
 
    -- ftdi_rxf_n is low when data is available
@@ -198,9 +215,13 @@ begin
    ram_we_n <= mem_wr_n;
    ram_oe_n <= mem_rd_n;
    
-   rom_ce_n <= '1';
-   rom_we_n <= '1';
-   rom_oe_n <= '1';
+   rom_ce_n <= rom_sel_n;
+   rom_we_n <= mem_wr_n;
+   rom_oe_n <= mem_rd_n;
+
+   rtc_ce_n <= not(sel1(2)) or iorq_n;
+   rtc_we_n <= io_wr_n;
+   rtc_oe_n <= io_rd_n;
 
    led(0) <= wait_n_i;
    led(1) <= a(0);
